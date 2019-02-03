f6b60ed037dbdfc04170cec0e716cc305e99718f
neutral-Stage <anirbanroy691@gmail.com>
2019-02-03 07:54:54 +0600

    First

---
+++ resources/js/components/users.vue
@@ -0,0 +1,118 @@
+<template>
+    <div class="container">
+        <div class="row mt-5">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Users Management Table</h3>
+
+                <div class="card-tools">
+                  <button class="btn btn-success" data-toggle="modal" data-target="#addNew">Add New <i class="fas fa-user-plus fa-fw"></i></button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <tbody><tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Type</th>
+                    <th>Modify</th>
+                  </tr>
+                  <tr>
+                    <td>183</td>
+                    <td>John Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-success">Approved</span></td>
+                    <td> <a href="#" @click="editModal(user)">
+                            <i class="fa fa-edit blue"></i>
+                        </a>
+                        /
+                        <a href="#" @click="deleteUser(user.id)">
+                            <i class="fa fa-trash red"></i>
+                        </a></td>
+                  </tr>
+                  
+                </tbody></table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+        <!-- Modal -->
+<div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addNewTitle">Add New User</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+                <div class="form-group">
+                <input v-model="form.name" type="text" name="name"
+                placeholder="Name"
+                    class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                <has-error :form="form" field="name"></has-error>
+                </div>
+
+                <div class="form-group">
+                <input v-model="form.email" type="text" email="email"
+                placeholder="Email"
+                    class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                <has-error :form="form" field="email"></has-error>
+                </div>
+
+                <div class="form-group">
+                        <select name="type" v-model="form.type" id="type" class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                            <option value="">Select User Role</option>
+                            <option value="admin">Admin</option>
+                            <option value="user">Standard User</option>
+                            <option value="author">Author</option>
+                        </select>
+                        <has-error :form="form" field="type"></has-error>
+                </div>
+
+                <div class="form-group">
+                        <input v-model="form.password" type="password" name="password" id="password"
+                        class="form-control" placeholder="Password" :class="{ 'is-invalid': form.errors.has('password') }">
+                        <has-error :form="form" field="password"></has-error>
+                </div>
+
+                
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn  btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" class="btn  btn-primary">Create</button>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        data () {
+    return {
+      // Create a new form instance
+      form: new Form({
+          id:'',
+          name : '',
+          email: '',
+          password: '',
+          type: '',
+          bio: '',
+          photo: ''
+      })
+    }
+  },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,161 @@
+
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>AdminLTE 3 | Starter</title>
+<!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- Font Awesome Icons -->
+<link rel="stylesheet" href="/css/app.css">
+
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper" id="app">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-align-justify"></i></a>
+      </li>
+     
+    </ul>
+
+    <!-- SEARCH FORM -->
+    <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fa fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+
+ 
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="./img/logo.png" alt="Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">Lara Start</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="./img/profile.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2" >
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+            <li class="nav-item">
+                <router-link to="/dashboard" class="nav-link">
+                <i class="nav-icon fas fa-tachometer-alt blue"></i>
+                <p>
+                    Dashboard
+                    
+                </p>
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link to="/profile" class="nav-link">
+                <i class="nav-icon fas fa-user cyan"></i>
+                <p>
+                    Profile
+                    
+                </p>
+                </router-link>
+            </li>
+            <li class="nav-item has-treeview ">
+                <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-cog green"></i>
+                <p>
+                    Management
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+                </a>
+                <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <router-link to="/users" class="nav-link">
+                    <i class="fas fa-users nav-icon purple"></i>
+                    <p>Users</p>
+                    </router-link>
+                </li>
+                
+                
+                </ul>
+            </li>
+            <li class="nav-item">
+               
+                  <a class="nav-link" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        <i class="nav-icon fas fa-power-off red"></i>
+                                        <p>
+                                              {{ __('Logout') }}
+                                        </p>
+                                        
+                                        
+                  </a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                  </form>
+                </li>
+            </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper" >
+    <div class="content-header">
+      <div class="container-fluid">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+
+<script src="/js/app.js"></script>
+</body>
+</html>
